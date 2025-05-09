a43f899710f5ee050c88a6c383ca17670925ff8d
Ahmed Hagag <100851728+hagag1919@users.noreply.github.com>
2025-05-10 00:23:45 +0300

    add project run on hagag machien

---
+++ system-order/src/main/java/com/example/systemorder/models/Dish.java
@@ -0,0 +1,63 @@
+package com.example.systemorder.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+
+
+@Entity
+@Table(name = "dishes")
+public class Dish {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String description;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Order getOrder() {
+        return order;
+    }
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    public Integer getAmount() {
+        return amount;
+    }
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+    
+}
+
