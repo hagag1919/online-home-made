9b0e5e7c38e94a81a5f232573268ebed4650a3b7
Ahmed Bakry <ahmedbakry024@gmail.com>
2025-05-15 21:16:30 +0300

    Update Dish.java

--- user/src/main/java/com/example/user/models/Dish.java
+++ user/src/main/java/com/example/user/models/Dish.java
@@ -10,15 +10,17 @@ public class Dish {
     private String description;
     private double price;
     private Long restaurantId;
+    private int amount;
 
     public Dish() {
     }
 
-    public Dish(Long id, String name, String description, double price, Long restaurantId) {
+    public Dish(Long id, String name, String description, double price, Long restaurantId, int amount) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.restaurantId = restaurantId;
+        this.amount = amount;
     }
 }
